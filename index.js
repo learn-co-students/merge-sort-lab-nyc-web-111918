73e3b3f1c36b0a625d68c16f9f47ada1870528ac
lumrachele <rlum894@gmail.com>
2019-06-01 15:55:56 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function findMinAndRemoveSorted(array){
+  return array.shift()
+}
 
+function merge(first, second){
+  let sorted=[]
+  while(first.length !== 0 && second.length !==0){
+    if(first[0]<second[0]){
+      sorted.push(findMinAndRemoveSorted(first))
+    }else{
+      sorted.push(findMinAndRemoveSorted(second))
+    }
+  }
+  return sorted.concat(first).concat(second)
+}
+
+function mergeSort(array){
+  let mid = array.length/2
+  let firstHalf = array.slice(0, mid)
+  let secondHalf = array.slice(mid, array.length)
+
+  if(array.length < 2){
+    return array
+  }else{
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+}
